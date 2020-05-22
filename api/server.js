634fb3347ef542b4390c22bb337971941f080bf8
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-22 09:31:09 -0700

    authenticate users to see jokes

--- api/server.js
+++ api/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
 
-const authenticate = require("../auth/authenticate-middleware.js");
+const { authenticate } = require("../auth/authenticate-middleware.js");
 const authRouter = require("../auth/auth-router.js");
 const jokesRouter = require("../jokes/jokes-router.js");
 const usersRouter = require("../users/user-router");
@@ -12,10 +13,11 @@ const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(cookieParser());
 
 server.use("/api/auth", authRouter);
 server.use("/api/users", usersRouter);
-server.use("/api/jokes", authenticate, jokesRouter);
+server.use("/api/jokes", authenticate(), jokesRouter);
 
 server.get("/", (req, res) => {
   res.json({ message: "up" });
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,33 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require("jsonwebtoken");
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+module.exports = {
+  authenticate
 };
+
+function authenticate() {
+  return async (req, res, next) => {
+    const authError = {
+      message: "Invalid Credentials"
+    };
+    try {
+      const token = req.cookies.token;
+      if (!token) {
+        res.status(401).json({ you: "shall not pass!" });
+      }
+      console.log(token);
+      jwt.verify(token, process.env.TOKEN_SECRET, (err, decodedPayload) => {
+        // if (err) {
+        // console.log(err)
+        //   return res.json(401).json(authError);
+        // }
+        req.token = decodedPayload;
+        next();
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
+}
