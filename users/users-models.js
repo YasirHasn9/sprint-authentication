17ead9bb79a0587bb45f1bc621cc82a065f02a91
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-22 08:40:44 -0700

    add function for new user

--- users/users-models.js
+++ users/users-models.js
@@ -1,8 +1,21 @@
 const db = require("../database/dbConfig");
 module.exports = {
-  find
+  find,
+  add
 };
 
 function find() {
   return db("users");
+}
+
+async function add(filter) {
+  try {
+    const [id] = await db("users").insert(add);
+    return db("users")
+      .where({ id })
+      .first();
+  } catch (err) {
+    console.log("ADD", err);
+    throw err;
+  }
 }
